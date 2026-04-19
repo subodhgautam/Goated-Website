459be62c799df0bd548583b04dd0f92f590f1112
subodhgautam <subodhgtm08@gmail.com>
2026-04-19 17:33:49 +0545

    Beautiful button and local storage setup

---
+++ script.js
@@ -0,0 +1,18 @@
+const goat_click_btn = document.getElementById("goat_click_btn")
+const balance_span = document.getElementById("balance_span")
+
+
+let balance = JSON.parse(localStorage.getItem("balance")) || 0 ;
+
+goat_click_btn.addEventListener("click", (e)=>{
+    e.preventDefault()
+    balance += 2;
+    updateBalance()
+})
+
+function updateBalance() {
+    balance_span.textContent = balance
+    localStorage.setItem("balance",JSON.stringify(balance))
+}
+
+updateBalance()
